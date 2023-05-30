edb8d64858287ab649aaee3b8839373448595567
Inna 612 <inna_661@mail.ru>
2023-05-30 09:23:47 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 12_728;
+
+        int bonus = price / 20;
+        System.out.println("Бонусы: " + bonus);
+
+    }
+
+}
